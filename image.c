e97e6cb85c9a88879bcc0b4d45887cbe569ff647
Hongtaewon <xodnjs1245@gmail.com>
2015-11-17 13:48:34 +0900

    version1

--- image.c
+++ image.c
@@ -31,15 +31,17 @@ int main(int argc, char *argv[]) {
   GtkWidget *window,*window1,*hbox;
   GtkWidget *image,*frame;
   GtkWidget *button1,*button2,*button3;
-
+  GtkWidget *table;
 
   //초기화 설정
   gtk_init(&argc, &argv);
-  
+
   //대화상자 설정
   window1 = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-
   gtk_window_set_title(GTK_WINDOW(window1), "Red Rock");
+  //테이블 생성
+  table = gtk_table_new(2,3,TRUE);
+
 
   image = gtk_image_new_from_file("image.jpg");
 
@@ -52,9 +54,13 @@ int main(int argc, char *argv[]) {
                 pack_button (hbox,"1",morning);
                 pack_button (hbox,"2",afternoon);
                 pack_button (hbox,"3",evening);
+
+  gtk_table_attach_defaults(GTK_TABLE(table),image,0,2,0,3);
+  gtk_table_attach_defaults(GTK_TABLE(table),hbox,2,3,0,1);
+
   //프레임 생성
-  gtk_container_add(GTK_CONTAINER(hbox),image);
-  gtk_container_add(GTK_CONTAINER(window1), hbox);
+  //gtk_container_add(GTK_CONTAINER(hbox),image);
+  gtk_container_add(GTK_CONTAINER(window1), table);
 
 
   gtk_widget_show_all(window1);
